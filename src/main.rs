cacb7473662a1d66c9d605b08126f3d0bb66edfc
AmeKnite <104745335+ameknite@users.noreply.github.com>
2024-01-19 12:53:56 -0600

    update something

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use std::cell::RefCell;
-thread_local! {
-    pub static FOO: RefCell<u32> = RefCell::new(1);
+use color_eyre::eyre;
+use itertools::Itertools;
+use tap::prelude::*;
 
-    static BAR: RefCell<f32> = RefCell::new(1.0);
-}
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
 
-// fn main() {
-//     FOO.with(|foo| assert_eq!(*foo.borrow(), 1));
-//     BAR.with(|bar| assert_eq!(*bar.borrow(), 1.0));
-// }
-fn main() {
-    println!("Hello world")
+    println!("hello, world");
+
+    Ok(())
 }
